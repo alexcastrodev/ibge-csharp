85094ec6b51d001a79d1c79e0f31859acd0d6bbd
Alex Oliveira <alexoliveira7x@gmail.com>
2023-10-16 19:11:16 +0100

    test: add more testing

--- Tests/LocationsControllerTests.cs
+++ Tests/LocationsControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using ibge.Controllers;
 using ibge.Dtos;
+using ibge.Models;
 using ibge.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -33,6 +34,49 @@ public class LocationTest
 		LocationSearchCriteria searchCriteria = new();
 		
 		// Act
+		var result = await _controller.Get(searchCriteria);
+
+		// Assert
+		Assert.AreEqual(0, result.Value?.Count);
+	}
+	
+	[TestMethod]
+	public async Task Post_Locations_ReturnsLocations()
+	{
+		// arrange
+		LocationSearchCriteria searchCriteria = new();
+		Location location = new()
+		{
+			Id = 1100015,
+			City = "Alta Floresta D'Oeste",
+			State = "RO",
+		};
+		
+		// Act
+		await _controller.Create(location);
+		var result = await _controller.Get(searchCriteria);
+
+		// Assert
+		Assert.AreEqual(1, result.Value?.Count);
+	}
+	
+	[TestMethod]
+	public async Task Get_LocationsWithCriteria_ReturnsEmpty()
+	{
+		// arrange
+		LocationSearchCriteria searchCriteria = new()
+		{
+			Id = 1100010,
+		};
+		Location location = new()
+		{
+			Id = 1100015,
+			City = "Alta Floresta D'Oeste",
+			State = "RO",
+		};
+		
+		// Act
+		await _controller.Create(location);
 		var result = await _controller.Get(searchCriteria);
 
 		// Assert
